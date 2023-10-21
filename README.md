d85f3da4e2cc580fcde7b8e9ae9e6668da934a61
Link <gabrielhmorais98@gmail.com>
2023-10-21 11:05:07 -0300

    initial commmit

---
+++ README.md
@@ -0,0 +1,116 @@
+
+<h1 align="center">Nmap auto scan</h1>
+
+<div align="center">
+
+[![Status](https://img.shields.io/badge/status-active-success.svg)]()
+[![GitHub Issues](https://img.shields.io/github/issues/kylelobo/The-Documentation-Compendium.svg)](https://github.com/kylelobo/The-Documentation-Compendium/issues)
+[![GitHub Pull Requests](https://img.shields.io/github/issues-pr/kylelobo/The-Documentation-Compendium.svg)](https://github.com/kylelobo/The-Documentation-Compendium/pulls)
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](/LICENSE)
+
+</div>
+
+---
+
+<p align="center"> A shell script to make easier use different Nmap features.
+  
+</p>
+
+## 📝 Table of Contents
+
+- [About](#about)
+- [Getting Started](#getting_started)
+- [Deployment](#deployment)
+- [Usage](#usage)
+- [Built Using](#built_using)
+- [Contributing](../CONTRIBUTING.md)
+- [Authors](#authors)
+
+
+## 🧐 About <a name = "about"></a>
+
+With Nmap Auto Scan you are able to use Nmap for differents type of scan such:
+  <ul>
+    <li> Host scan;
+    <li> Default script scan;
+    <li> Service scan;
+    <li> Os scan;
+  </ul>
+
+And also use useful features like:
+
+  <ul>
+    <li> Scripts:
+      <ul>
+        <li> Vuln;
+        <li> Auth;
+      </ul>
+    <li> Decoy:
+    <ul>
+      <li> Private IP;
+      <li> Random public IP;
+    </ul>
+    <li> Output;
+  </ul>
+
+Without the need to set evertyhing manualy and providing a clear output.
+
+## 🏁 Getting Started <a name = "getting_started"></a>
+
+### Prerequisites
+<ul>
+  <li>You need to have Nmap installed in your machine.
+  <li>Some features of Nmap requires Root permission.
+</ul>
+
+### Installing
+
+If you don't have Nmap installed in your computer, just copy the bellow codes to install. Otherwise just skip this step.
+
+```
+sudo apt update && sudo apt-get install nmap -y
+```
+
+
+
+```
+git clone https://github.com/L1nk404/auto-nmap-scan 
+cd auto-nmap-scan
+chmod +x scan.sh
+./scan.sh
+```
+
+
+## 🔧 Setting up <a name = "setting"></a>
+
+Now it's time to change the code for your own necessities.
+
+### Change the default path for output (Mandatory):
+
+In line 116 you have to change the default path. DONT change the variable "$folder".
+
+```
+# CHANGE THE PATH!!
+cd /set/your/default/path/"$folder" || echo -e "\e[1m\e[31Path not found, try again.\e[0m\e[0m" | exit
+```
+
+### Changing the private ip for decoy (optional):
+
+In line 120 you have to change your private ip if you want to use the Decoy. Like the following example:
+
+```
+# PRIVATES IPS! CHANGE IT FOR YOUR PREFENCE
+
+# my_ip='10.2.65'
+# spoof_ips="-D $my_ip.192 $my_ip.87 $my_ip.183 $my_ip.213 $my_ip.84 $my_ip.182 $my_ip.23 $my_ip.145"
+
+```
+
+### You can also change or add the Nmap scan for fit your necessities.
+
+
+
+## ✍️ Authors <a name = "authors"></a>
+
+- [@l1nk](https://github.com/l1nk404) - Idea & Initial work
+
---
+++ scan.sh
@@ -0,0 +1,221 @@
+#!/bin/bash
+
+########################################################################
+
+ASCIIART="
+                                             :!JPGBGY7^.                                            
+                                         .7G&@@@@@@@@@@@&5^                                         
+                                       ^G@@@@BJ~:.?B?!5&@@@&J.                                      
+                                     ^#@@@B!.     .P&G: .J&@@@Y.                                    
+                                   .P@@@G:        ...Y@#~  7&@@@!                                   
+                                  ~&@@&~          7@B:.J&&!  Y@@@G.                                 
+                                 J@@@P.            .Y@#^.7&&7 :&@@&:                                
+                                P@@@?             7G:.J&#~ !&&?:G@@@~                               
+                               B@@@~              .5@#^.7&&7 ~#&5B@@@7                              
+                             .#@@@^               ...J&#~ !&&?.^B@@@@@7                             
+                             B@@@:                7&B^.?&&! ~#@J.:P@@@@7                            
+                            B@@@:          ........^G@#^.?&&7.^B&Y..P@@@!                           
+                           P@@@^ .^!?5GB#&&&@@@@@@@@@@@@@&&@@&5JP@@5:P@@@^                          
+                          Y@@@&#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#@@@@:                         
+                         7@@@@@@@@@@@@GJJJJJJJJJJJJJJJJJJJJJJY#@@@@@@@@@@@&.                        
+                        ^@@@@@@@@@@@@@.           ^.          5@@@@@@@@@@@@B                        
+                        7@@@@@@@@@@@@@G           J&.        :@@@@@@@@@@@@@@.                       
+                         5@@@@@@@@@@@@@!          ~@5        B@@@@@@@@@@@@&^                        
+                          ^#@@@@@@@@@@@@:           .       Y@@@@@@@@@@@@Y.                         
+                            ^B@@@@@@@@@@&.                 ?@@@@@@@@@@@J.                           
+                           .~G@@@@@@@@@@@@Y:            .!B@@@@@@@@@@@&J:                           
+                        .!G@@@@@@@@@@@@@@@@@&GJ!^:.:~75#@@@@@@@@@@@@@@@@@&Y^                        
+                     .7B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&5^                     
+                   :G@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&J.                  
+                  J@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&:                 
+                 J@@@@@@BJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJY@@@@@@@.                
+                !@@@@@@@^                                                   G@@@@@@&.               
+               ^@@@@@@@@~                      ..:::.                       B@@@@@@@G               
+              .&@@@@@@@@!                  .7G&@@@@@@&G7.                   #@@@@@@@@Y              
+              #@@@@@@@@@?                .Y@@@@@@@@@@@@@@5.                 &@@@@@@@@@7             
+             P@@@@@@@@@@Y               .&@@@@@@@@@@@@@@@@&.               .&@@@@@@@@@@^            
+            J@@@@@@@@@@@5               G@@@@@@@@@@@@@@@@@@B               .@@@@@@@@@@@@.           
+           ^@@@@@@@@@@@@G               &@@GB&&@@@@@@@&BG@@@.              .@@@@@@@@@@@@B           
+           G@@@@@@@@@@@@B               B@@.    !@@7    .@@#               :@@@@@@@@@@@@@^          
+           #@@@@@@@@@@@@#               .&@&J!!Y@@@@5!!J&@&:               ^@@@@@@@@@@@@@!          
+           J@@@@@@@@@@@@&                :@@@@@@?P&~@@@@@@^                ~@@@@@@@@@@@@@.          
+            G@@@@@@@@@@@&                !@@@@@@Y&@Y&@@@@@7                !@@@@@@@@@@@@!           
+             Y@@@@@@@@@@@.                ^~^G@@@@@@@@B^~^                 ?@@@@@@@@@@&^            
+              :G@@@@@@@@@.                   ~&&&&&&&&!                    Y@@@@@@@@&J              
+   .JGGGGGGGGGGG@@@@@@@@@.                                                 5@@@@@@@@#PGGGGGGGGGY.   
+   .&@@@@@@@@@@@@@@@@@@@@!:::::::::::::::::::::::::::::::::::::::::::::::::B@@@@@@@@@@@@@@@@@@@&:   
+     ...............7&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@G:..............     
+                     ^@@@G?JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ&@@#                     
+                     :@@@B5555555555555555555555555555555555555555555555555@@@B                     
+                     .#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@?                     
+                       .......................................................       
+
+
+                    __   _ _______ _______  _____       _______ _______ _______ __   _
+                    | \  | |  |  | |_____| |_____]      |______ |       |_____| | \  |
+                    |  \_| |  |  | |     | |            ______| |_____  |     | |  \_|
+
+
+                                                                                            
+"
+
+#######################################################################################################
+
+BANNER () {
+    echo -e "\e[36m$ASCIIART\e[0m"
+
+    echo -e "\e[1m\e[31m################### DONT FORGET TO RUN IN ROOT #############################\e[0m\e[0m"
+    echo
+    echo
+}
+
+main_header() {
+
+  message="\e[32m\e[1m[+] \e[0m\e[0m $1"
+  echo -e -n "$message"
+}
+
+scan_type_header() {
+  echo
+  message="\e[32m\e[1m[>>]$1\e[0m\e[0m"
+  echo -e "$message"
+}
+
+
+echo_done(){
+  message="\e[32m\e[1m[=]Done!\e[0m\e[0m"
+  echo -e "$message"
+  echo 
+}
+
+
+
+
+#########################################################################################################
+clear 
+
+BANNER
+
+main_header "Type target addrs: "
+read -r ip_addr
+
+main_header "Do host scan: [y/n] "
+read -r host_scan
+
+main_header "Assume alive: [y/n] "
+read -r alive
+
+main_header "Use Decoy?: [y/n]"
+read -r decoy
+
+main_header "Enter the folder for save log files: "
+read -r folder
+
+
+# CHANGE THE PATH!!
+cd /home/link/Desktop/HackLogs/TryHackMe/"$folder" || echo -e "\e[1m\e[31Path not found, try again.\e[0m\e[0m" | exit
+
+
+
+# PRIVATES IPS! CHANGE IT FOR YOUR PREFENCE
+
+# my_ip='10.2.65'
+# decoy_ips="-D $my_ip.192 $my_ip.87 $my_ip.183 $my_ip.213 $my_ip.84 $my_ip.182 $my_ip.23 $my_ip.145"
+
+decoy_ips="-D RND:5"
+
+mkdir nmap
+cd nmap || exit
+
+
+if [ "$decoy" = "y" ];then
+
+  if [ "$host_scan" == "y" ];then  
+    scan_type_header "Host Scan: "
+    nmap "$decoy_ips" -sS -sU "$ip_addr"  -oN host.txt
+    echo_done
+  fi
+
+  if [ "$alive" == "y" ];then
+    scan_type_header "Default scan: "
+    nmap "$decoy_ips" -sC -sV -Pn  "$ip_addr" -oN default.txt
+    echo_done
+
+    scan_type_header "Os scan: "
+    nmap "$decoy_ips" -O -Pn "$ip_addr" -oN os.txt
+    echo_done
+
+    scan_type_header "Vuln scan: "
+    nmap "$decoy_ips" --script vuln -Pn "$ip_addr" -oN vuln.txt
+    echo_done
+
+    scan_type_header "Auth scan: "
+    nmap "$decoy_ips" --script auth -Pn "$ip_addr" -oN auth.txt
+    echo_done
+
+    
+
+  else
+    scan_type_header "Default scan: "
+    nmap "$decoy_ips" -sC -sV "$ip_addr" -oN default.txt
+    echo_done
+    
+    scan_type_header "Os scan: "
+    nmap "$decoy_ips" -O -Pn --system-dns "$ip_addr" -oN os.txt
+    echo_done
+
+    scan_type_header "Vuln scan: "
+    nmap "$decoy_ips" --script vuln "$ip_addr" -oN vuln.txt
+    echo_done
+
+    scan_type_header "Auth scan: "
+    nmap "$decoy_ips" --script auth "$ip_addr" -oN auth.txt
+    echo_done
+  fi
+
+else
+
+  if [ "$host_scan" == "y" ];then  
+    scan_type_header "Host Scan: "
+    nmap -sS -sU "$ip_addr" -oN /nmap/hosts.txt
+    echo_done
+  fi
+
+  if [ "$alive" == "y" ];then
+    scan_type_header "Default scan: "
+    nmap -sC -sV -Pn "$ip_addr" -oN default.txt
+    echo_done
+
+    scan_type_header "Os scan: "
+    nmap -O -Pn "$ip_addr" -oN os.txt
+    echo_done
+
+    scan_type_header "Vuln scan: "
+    nmap --script vuln -Pn "$ip_addr" -oN vuln.txt
+    echo_done
+
+    scan_type_header "Auth scan: "
+    nmap --script auth -Pn "$ip_addr" -oN auth.txt
+    echo_done
+
+    
+
+  else
+    scan_type_header "Default scan: "
+    nmap -sC -sV "$ip_addr" -oN default.txt
+    echo_done
+    
+    scan_type_header "Os scan: "
+    nmap -O -Pn "$ip_addr" -oN os.txt
+    echo_done
+
+    scan_type_header "Vuln scan: "
+    nmap --script vuln "$ip_addr" -oN vuln.txt
+    echo_done
+
+    scan_type_header "Auth scan: "
+    nmap --script auth= "$ip_addr" -oN auth.txt
+    echo_done
+  fi
+
+fi
