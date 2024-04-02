ee801d624e3065547823b1423ff387d2705e9ac9
l1nk404 <gabrielhmorais98@gmal.com>
2024-04-02 20:32:02 -0300

    New menu and features

--- oldScan.sh
+++ oldScan.sh
@@ -61,186 +61,160 @@ ASCIIART="
 
 #######################################################################################################
 
-BANNER () 
-{
+BANNER () {
     echo -e "\e[92m$ASCIIART\e[0m"
+
     echo -e "\e[1m\e[31m################### DONT FORGET TO RUN IN ROOT #############################\e[0m\e[0m"
     echo
     echo
 }
 
-main_header()
-{
-    message="\e[1m\e[38;5;21m$1\e[0m\e[0m"
-    echo -e "$message"
+main_header() {
+
+  message="\e[32m\e[1m[+] \e[0m\e[0m $1"
+  echo -e -n "$message"
 }
 
-scan_type_header()
-{
-    echo
-    message="\e[32m\e[1m[>>]$1\e[0m\e[0m"
-    echo -e "$message"
-}
-
-echo_done()
-{
-    "\e[32m\e[1m[=]Done!\e[0m\e[0m"
-    echo -e "$message"
-    echo 
-}
-
-################ VARIABLES FOR CONFIG #################################################
-
-decoy_ips="-D 5" # Change this option with you want
-
-target_addr=" "
-host_scan="False"
-assume_alive="True"
-timing="3" # Default Timing
-decoy="True" 
-path=" "  # You can set a default patch if you want
-
-#######################################################################################
-
-display_menu()
-{   
-    echo -e "[1] Target addrs: \e[38;5;45m$target_addr\e[0m"
-    echo -e "[2] Do host scan: \e[38;5;45m$host_scan\e[0m"
-    echo -e "[3] Assume alive: \e[38;5;45m$assume_alive\e[0m"
-    echo -e "[4] Decoy: \e[38;5;45m$decoy\e[0m"
-    echo -e "[5] Enter the timing: \e[38;5;45m$timing\e[0m"
-    echo -e "[6] Folder path for log files: \e[38;5;45m$path\e[0m"
-    echo -e "[0] Start Scan: "
-    
+scan_type_header() {
+  echo
+  message="\e[32m\e[1m[>>]$1\e[0m\e[0m"
+  echo -e "$message"
 }
 
 
-scan()
-{
-    if [[ "$decoy" == "True" ]] || [[ "$decoy" == "true" ]];then
-
-        if [[ "$host_scan" == "True" ]] || [[ "$host_scan" == "true" ]];then  
-            scan_type_header "Host Scan: "
-            nmap "$decoy_ips" -sS -sU "$target_addr" "$timing" -oN host.txt
-            echo_done
-        fi
-
-        if [[ "$assume_alive" == "True" ]] || [[ "$assume_alive" == "true" ]];then  
-                scan_type_header "Default scan: "
-                nmap "$decoy_ips" -sC -sV -Pn  "$target_addr" "$timing" -oN default.txt
-                echo_done
-
-                scan_type_header "Os scan: "
-                nmap "$decoy_ips" -O -Pn "$target_addr" "$timing" -oN os.txt
-                echo_done
-
-                scan_type_header "Vuln scan: "
-                nmap "$decoy_ips" --script vuln -Pn "$target_addr" "$timing" -oN vuln.txt
-                echo_done
-
-                scan_type_header "Auth scan: "
-                nmap "$decoy_ips" --script auth -Pn "$target_addr" "$timing" -oN auth.txt
-                echo_done
-        else # Not assume alive
-                scan_type_header "Default scan: "
-                nmap "$decoy_ips" -sC -sV "$target_addr" "$timing" -oN default.txt
-                echo_done
-
-                scan_type_header "Os scan: "
-                nmap "$decoy_ips" -O "$target_addr" "$timing" -oN os.txt
-                echo_done
-
-                scan_type_header "Vuln scan: "
-                nmap "$decoy_ips" --script vuln "$target_addr" "$timing" -oN vuln.txt
-                echo_done
-
-                scan_type_header "Auth scan: "
-                nmap "$decoy_ips" --script auth "$target_addr" "$timing" -oN auth.txt
-                echo_done
-        fi
-
-    else # Not decoy
-
-        if [[ "$host_scan" == "True" ]] || [[ "$host_scan" == "true" ]];then  
-            scan_type_header "Host Scan: "
-            nmap -sS -sU "$target_addr" "$timing" -oN host.txt
-            echo_done
-        fi
-
-        if [[ "$assume_alive" == "True" ]] || [[ "$assume_alive" == "true" ]];then  
-                scan_type_header "Default scan: "
-                nmap -sC -sV -Pn  "$target_addr" "$timing" -oN default.txt
-                echo_done
-
-                scan_type_header "Os scan: "
-                nmap -O -Pn "$target_addr" "$timing" -oN os.txt
-                echo_done
-
-                scan_type_header "Vuln scan: "
-                nmap --script vuln -Pn "$target_addr" "$timing" -oN vuln.txt
-                echo_done
-
-                scan_type_header "Auth scan: "
-                nmap --script auth -Pn "$target_addr" "$timing" -oN auth.txt
-                echo_done
-        else
-                scan_type_header "Default scan: "
-                nmap -sC -sV "$target_addr" "$timing" -oN default.txt
-                echo_done
-
-                scan_type_header "Os scan: "
-                nmap -O "$target_addr" "$timing" -oN os.txt
-                echo_done
-
-                scan_type_header "Vuln scan: "
-                nmap --script vuln "$target_addr" "$timing" -oN vuln.txt
-                echo_done
-
-                scan_type_header "Auth scan: "
-                nmap --script auth "$target_addr" "$timing" -oN auth.txt
-                echo_done
-        fi
-
-    fi
+echo_done(){
+  message="\e[32m\e[1m[=]Done!\e[0m\e[0m"
+  echo -e "$message"
+  echo 
 }
+
+
+
 
 #########################################################################################################
+clear 
+
+BANNER
+
+main_header "Type target addrs: "
+read -r ip_addr
+
+main_header "Do host scan: [y/n] "
+read -r host_scan
+
+main_header "Assume alive: [y/n] "
+read -r alive
+
+main_header "Use Decoy?: [y/n] "
+read -r decoy
+
+main_header "Enter the folder for save log files: "
+read -r folder
 
 
-# Main menu loop
-while true; do
-    clear
-    BANNER
-    display_menu
-    read -r -p "Enter the option to set or press 0 to run: " choice
-    echo ""
-    case $choice in 
-        1)
-            read -r -p "Set target address: " target_addr
-            ;;
-        2)  
-            read -r -p "Set True or False: " host_scan
-            ;;
-        3)  
-            read -r -p "Set True or False: " assume_alive
-            ;;
-        4)  
-            read -r -p "Set True or False: " assume_alive
-            ;;
-        5)
-            read -r -p "Set the timing: paranoid (0), sneaky (1), polite (2), normal (3), aggressive (4), and insane (5): " timing
-            ;;
-        6)
-            read -r -p "Enter the folder path for save the log files: "  path
-            ;;
-        0)
-            main_header "Scanning..."
-            scan
-            break
-            ;;         
-    esac
-done
+# CHANGE THE PATH!!
+cd /home/link/Desktop/HackLogs/TryHackMe/"$folder" || echo -e "\e[1m\e[31Path not found, try again.\e[0m\e[0m" | exit
 
 
 
+# PRIVATES IPS! CHANGE IT FOR YOUR PREFENCE
 
+# my_ip='10.2.65'
+# decoy_ips="-D $my_ip.192 $my_ip.87 $my_ip.183 $my_ip.213 $my_ip.84 $my_ip.182 $my_ip.23 $my_ip.145"
+
+decoy_ips="-D RND:5"
+
+mkdir -p nmap
+cd nmap || exit
+
+
+if [ "$decoy" = "y" ];then
+
+  if [ "$host_scan" == "y" ];then  
+    scan_type_header "Host Scan: "
+    nmap "$decoy_ips" -sS -sU "$ip_addr"  -oN host.txt
+    echo_done
+  fi
+
+  if [ "$alive" == "y" ];then
+    scan_type_header "Default scan: "
+    nmap "$decoy_ips" -sC -sV -Pn  "$ip_addr" -oN default.txt
+    echo_done
+
+    scan_type_header "Os scan: "
+    nmap "$decoy_ips" -O -Pn "$ip_addr" -oN os.txt
+    echo_done
+
+    scan_type_header "Vuln scan: "
+    nmap "$decoy_ips" --script vuln -Pn "$ip_addr" -oN vuln.txt
+    echo_done
+
+    scan_type_header "Auth scan: "
+    nmap "$decoy_ips" --script auth -Pn "$ip_addr" -oN auth.txt
+    echo_done
+    
+
+  else
+    scan_type_header "Default scan: "
+    nmap "$decoy_ips" -sC -sV "$ip_addr" -oN default.txt
+    echo_done
+    
+    scan_type_header "Os scan: "
+    nmap "$decoy_ips" -O -Pn --system-dns "$ip_addr" -oN os.txt
+    echo_done
+
+    scan_type_header "Vuln scan: "
+    nmap "$decoy_ips" --script vuln "$ip_addr" -oN vuln.txt
+    echo_done
+
+    scan_type_header "Auth scan: "
+    nmap "$decoy_ips" --script auth "$ip_addr" -oN auth.txt
+    echo_done
+  fi
+
+else
+
+  if [ "$host_scan" == "y" ];then  
+    scan_type_header "Host Scan: "
+    nmap -sS -sU "$ip_addr" -oN /nmap/hosts.txt
+    echo_done
+  fi
+
+  if [ "$alive" == "y" ];then
+    scan_type_header "Default scan: "
+    nmap -sC -sV -Pn "$ip_addr" -oN default.txt
+    echo_done
+
+    scan_type_header "Os scan: "
+    nmap -O -Pn "$ip_addr" -oN os.txt
+    echo_done
+
+    scan_type_header "Vuln scan: "
+    nmap --script vuln -Pn "$ip_addr" -oN vuln.txt
+    echo_done
+
+    scan_type_header "Auth scan: "
+    nmap --script auth -Pn "$ip_addr" -oN auth.txt
+    echo_done
+
+    
+
+  else
+    scan_type_header "Default scan: "
+    nmap -sC -sV "$ip_addr" -oN default.txt
+    echo_done
+    
+    scan_type_header "Os scan: "
+    nmap -O -Pn "$ip_addr" -oN os.txt
+    echo_done
+
+    scan_type_header "Vuln scan: "
+    nmap --script vuln "$ip_addr" -oN vuln.txt
+    echo_done
+
+    scan_type_header "Auth scan: "
+    nmap --script auth= "$ip_addr" -oN auth.txt
+    echo_done
+  fi
+
+fi
